889a17080a578e03501326559bad0984c004a7c2
unknown <mandolsuk@gmail.com>
2022-03-21 20:16:47 +0900

    - test git

---
+++ salesanalysis.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 21 20:13:44 2022
+
+@author: Jack
+"""
+
+import pandas as pd
+
+# 인덱스를 지정해 시트 설정
+df_sheet_index = pd.read_excel('C:/Users/Administrator/Desktop/python/excel/excel_sample.xlsx', sheet_name=1)
