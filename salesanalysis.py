85720d197c93fd1249508fe40b8440e821faa921
unknown <mandolsuk@gmail.com>
2022-03-26 21:35:13 +0900

    ver0.1(done)    - arrange total menu

--- salesanalysis.py
+++ salesanalysis.py
@@ -23,16 +23,35 @@ file_path = "D:\workspace\exelFiles"
 file_list = glob.glob(f"{file_path}/*{file_format}")
 
 
+total_dic = {}
+total_list = []
+
 for i in range(len(file_list)):
     globals()['df_'+str(i)] = pd.read_excel(file_list[i], skiprows=[0,1], index_col = '메뉴코드')
     globals()['dic_'+str(i)] =globals()['df_'+str(i)].to_dict('index')
 #for i in range(len(file_list)):
     
-print(dic_0['C0000003']['매출건수'])
+#print(dic_0['C0000003']['매출건수'])
 #file_df = pd.read_excel(file_list[0], skiprows=[0,1], index_col = '메뉴코드')
 #df_dic = file_df.to_dict('index')
 #df_dic_list[0] = df_dic
 
 for i in range(len(file_list)):
-    for key in globals()['dic_'+str(i)].keys():
-        print(key)
+    temp_dic = globals()['dic_'+str(i)]
+    for key in temp_dic.keys():
+        if key in total_dic:
+#            print("sum value")
+            total_dic[key]['매출건수'] = total_dic[key]['매출건수'] + temp_dic[key]['매출건수']
+            total_dic[key]['매출금액'] = total_dic[key]['매출금액'] + temp_dic[key]['매출금액']
+            total_dic[key]['부가세'] = total_dic[key]['부가세'] + temp_dic[key]['부가세']
+            total_dic[key]['순매출'] = total_dic[key]['순매출'] + temp_dic[key]['순매출']
+            total_dic[key]['실매출'] = total_dic[key]['실매출'] + temp_dic[key]['실매출']
+            total_dic[key]['판매건수'] = total_dic[key]['판매건수'] + temp_dic[key]['판매건수']
+            total_dic[key]['판매수량'] = total_dic[key]['판매수량'] + temp_dic[key]['판매수량']
+        else :
+#            print("add dic")
+            total_dic[key] = temp_dic[key]
+            
+
+total_df = pd.DataFrame(total_dic)
+total_df.to_excel("output.xlsx")
